335181cd81d80840bafd58530bd12b682c83d9dc
Роман Павлов <55055449+Crunchy0@users.noreply.github.com>
2020-10-21 10:29:57 +0300

    lab2 - Pavlov

--- Pavlov/lab2/main.cpp
+++ Pavlov/lab2/main.cpp
@@ -1,4 +1,4 @@
-#include <string>
+﻿#include <string>
 #include <iostream>
 #include <fstream>
 
@@ -23,6 +23,12 @@ struct elem {
 	} n;
 };
 
+void indent(int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "\t";
+	}
+}
+
 
 //Создание атома и заполнение его полей
 elem* create_atom(char c) {
@@ -45,36 +51,64 @@ elem* create_node() {
 
 
 //Генерация списка
-elem *create_list(const string &list, int &i) {
-	elem* n_list = create_node();
-	elem* current = n_list;
-	while (i < list.length()) {
-		if (list[i] == '(') {
-			//Создание подсписка, переход к след. узлу
-			current->n.pair.child = create_list(list, ++i);
-			current->n.pair.next = create_node();
-			current = current->n.pair.next;
-		}
-		else if (list[i] == ' ' || list[i] == '\n' || list[i] == 'r' || list[i] == '\t') {
-			//Игнорировать пробельные символы
-			i++;
-		}
-		else if (list[i] == ')' || list[i] == '\0') {
-			//Выход из цикла и завершение функции
-			i++;
-			break;
-		}
-		else {
-			//Прикрепление к узлу атома, переход к след. узлу
-			current->n.pair.child = create_atom(list[i]);
-			if(list[i + 1] != ')'){
-				current->n.pair.next = create_node();
-			}
-			current = current->n.pair.next;
-			i++;
-		}
+elem* create_list(const string& list, int& i, int &depth) {
+	if (list.length() == 1) {
+		cout << "Начата генерация списка\n";
+		elem* n_list = create_atom(list[0]);
+		cout << "В список занесён атом : " << list[0] << " (Список сам является атомом)\n";
+		return n_list;
 	}
-	return n_list;
+	else {
+		elem* n_list = create_node();
+		elem* current = n_list;
+		while (i < list.length()) {
+			if (list[i] == '(') {
+				//Создание подсписка, переход к след. узлу
+				if (!i) {
+					cout << "Начата генерация списка\n";
+					indent(depth);
+					cout << "Рассматриваются скобки " << depth << " уровня : " << list[i] << "\n";
+					i++;
+				}
+				else {
+					depth++;
+					indent(depth);
+					cout << "Рассматриваются скобки " << depth << " уровня : " << list[i] << "\n";
+					current->n.pair.child = create_list(list, ++i, depth);
+					current->n.pair.next = create_node();
+					current = current->n.pair.next;
+				}
+			}
+			else if (list[i] == ' ' || list[i] == '\n' || list[i] == 'r' || list[i] == '\t') {
+				//Игнорировать пробельные символы
+				i++;
+			}
+			else if (list[i] == ')' || list[i] == '\0') {
+				//Выход из цикла и завершение функции
+				if (list[i] == ')') {
+					indent(depth);
+					cout << "Закончено рассмотрение скобок " << depth << " уровня : " << list[i] << "\n";
+				}
+				if (depth > 0) {
+					depth--;
+				}
+				i++;
+				break;
+			}
+			else {
+				//Прикрепление к узлу атома, переход к след. узлу
+				current->n.pair.child = create_atom(list[i]);
+				if (list[i + 1] != ')') {
+					current->n.pair.next = create_node();
+				}
+				indent(depth);
+				cout << "В список занесён атом : " << list[i] << "\n";
+				current = current->n.pair.next;
+				i++;
+			}
+		}
+		return n_list;
+	}
 }
 
 //Поиск последнего узла на текущем уровне
@@ -86,21 +120,38 @@ elem* last_node(elem* list) {
 }
 
 //Оборачивание списка
-elem* reverse_list(elem *list) {
+elem* reverse_list(elem* list, int & depth) {
 	if (list != nullptr) {
 		elem* new_head = list;
 		if (!list->atom) {
 			//Сохраняем голову, рекурсивно обходим след. элементы
 			//и меняем направление списка на текущем уровне на обратное
 			new_head = last_node(list);
-			if (list->n.pair.child != nullptr && !(list->n.pair.child->atom)) {
-				list->n.pair.child = reverse_list(list->n.pair.child);
-			}
 			if (list->n.pair.next != nullptr) {
-				reverse_list(list->n.pair.next);
+				reverse_list(list->n.pair.next, depth);
 				list->n.pair.next->n.pair.next = list;
 				list->n.pair.next = nullptr;
 			}
+			if (list->n.pair.child != nullptr){
+				if (!list->n.pair.child->atom) {
+					indent(depth);
+					cout << "Узел с подсписком, спуск на 1 уровень\n";
+					depth++;
+					list->n.pair.child = reverse_list(list->n.pair.child, depth);
+					depth--;
+				}
+				else {
+					indent(depth);
+					cout << "Узел с атомом : " << list->n.pair.child->n.content << "\n";
+				}
+			}
+			if(list->n.pair.child == nullptr && list->n.pair.next == nullptr) {
+				indent(depth);
+				cout << "Пустой узел\n";
+			}
+		}
+		else {
+			cout << "Список состоит из одного атома (без узлов) - " << list->n.content << " - и не нуждается в инвертировании\n";
 		}
 		return new_head;
 	}
@@ -108,23 +159,26 @@ elem* reverse_list(elem *list) {
 }
 
 //Вывод списка на экран
-void print_list(elem* list, int &depth) {
+void print_list(elem* list, int& depth) {
 	if (list != nullptr) {
 		if (!list->atom) {
 			if (list->n.pair.child != nullptr) {
 				depth++;
+				if (!list->n.pair.child->atom) {
+					cout << "(";
+				}
 				print_list(list->n.pair.child, depth);
+				if (!list->n.pair.child->atom) {
+					cout << ")";
+				}
 				depth--;
 			}
-			if(list->n.pair.next != nullptr){
+			if (list->n.pair.next != nullptr) {
 				print_list(list->n.pair.next, depth);
 			}
 		}
 		else {
-			for (int n = 0; n < depth; n++) {
-				cout << "\t";
-			}
-			cout << list->n.content << "\n";
+			cout << list->n.content;
 		}
 	}
 }
@@ -132,11 +186,11 @@ void print_list(elem* list, int &depth) {
 //Удаление списка
 void delete_list(elem* list) {
 	if (list != nullptr) {
-		if(!list->atom){
+		if (!list->atom) {
 			if (list->n.pair.child != nullptr) {
 				delete_list(list->n.pair.child);
 			}
-			if(list->n.pair.next != nullptr){
+			if (list->n.pair.next != nullptr) {
 				delete_list(list->n.pair.next);
 			}
 		}
@@ -145,7 +199,7 @@ void delete_list(elem* list) {
 }
 
 //Запись заданной строки в файл с указанным именем
-void write_to_file(const string filename, const string arg){
+void write_to_file(const string filename, const string arg) {
 	ofstream output;
 	output.open(filename, ios::app);
 	output << arg;
@@ -188,32 +242,37 @@ void wrapper(elem* list, elem* rev_list, string filename) {
 }
 
 //Анализатор выражения
-bool is_entry_valid(const string &list, int &i) {
-	if (!i && list[i] != '(') {
-		//Скобка не стоит в начале
-		return false;
-	}
-	else if (list[i] == '(') {
-		//Проверка каждого выражения со скобками
-		i++;
-		while (list[i] != ')') {
-			if (list[i] == '\0' || !is_entry_valid(list, i)) {
-				return false;
-			}
-			else {
-				i++;
-			}
-		}
+bool is_entry_valid(const string& list, int& i) {
+	if (list.length() == 1 && list[0] != '(' && list[0] != ')') {
 		return true;
 	}
 	else {
-		//Встречен любой символ, кроме "("
-		return true;
+		if (!i && list[i] != '(') {
+			//Скобка не стоит в начале
+			return false;
+		}
+		else if (list[i] == '(') {
+			//Проверка каждого выражения со скобками
+			i++;
+			while (list[i] != ')') {
+				if (list[i] == '\0' || !is_entry_valid(list, i)) {
+					return false;
+				}
+				else {
+					i++;
+				}
+			}
+			return true;
+		}
+		else {
+			//Встречен любой символ, кроме "("
+			return true;
+		}
 	}
 }
 
 int main() {
-	elem* list = nullptr, *reversed_list = nullptr;
+	elem* list = nullptr, * reversed_list = nullptr;
 	string s = "", input_filename = "";
 	const string output_filename = "output.txt";
 	ofstream output;
@@ -234,30 +293,55 @@ int main() {
 		if (input.is_open()) {
 			getline(input, s);
 			do {
+				cout << s << "\n\n";
+				i = 0;
 				if (is_entry_valid(s, i)) {
 					//Создаём исходный и обращённый списки
-					i = 1;
-					list = create_list(s, i);
-					i = 1;
-					reversed_list = reverse_list(create_list(s, i));
+					i = 0;
+					list = create_list(s, i, depth);
+					cout << "\nПовторная генерация списка(для хранения обращённого списка)\n";
+					i = 0;
+					cout << "\n";
+					reversed_list = reverse_list(create_list(s, i, depth), depth);
 					//Выводим их в файл
-					wrapper(list, reversed_list, output_filename);
-					cout << "\n";
+					if (s.length() == 1) {
+						print_list_to_file(list, output_filename);
+						write_to_file(output_filename, " -> ");
+						print_list_to_file(reversed_list, output_filename);
+						write_to_file(output_filename, "\n");
+					}
+					else {
+						wrapper(list, reversed_list, output_filename);
+					}
+					cout << "\nИтоговый ответ: ";
+					if (s.length() != 1) {
+						cout << "(";
+					}
 					print_list(list, depth);
-					cout << "\n";
+					if (s.length() == 1) {
+						cout << " -> ";
+					}
+					else {
+						cout << ") -> (";
+					}
 					print_list(reversed_list, depth);
+					if (s.length() != 1) {
+						cout << ")\n\n";
+					}
+					else {
+						cout << "\n\n";
+					}
 					//Удаляем их
 					delete_list(list);
 					delete_list(reversed_list);
 				}
-				else{
+				else {
 					//Записываем в файл сообщение об ошибке
 					write_to_file(output_filename, s);
-					write_to_file(output_filename, " -  INVALID ENTRY !\n");	
+					write_to_file(output_filename, " -  INVALID ENTRY !\n");
 				}
 				s = "";
 				getline(input, s);
-				i = 0;
 			} while (!input.eof());
 			input.close();
 		}
@@ -265,33 +349,49 @@ int main() {
 			cout << "В текущей директории нет файла с таким названием!\n";
 		}
 	}
-	else if(command == 1){
+	else if (command == 1) {
 		cout << "Введите список (для выхода введите \"!\" ) :\n\n";
 		getline(cin, s);
 		while (s != "!") {
+			cout << "\n";
+			i = 0;
 			if (is_entry_valid(s, i)) {
 				//Процесс аналогичен предыдущему, но нет вывода в файл и ввод производится вручную из cin
-				i = 1;
-				list = create_list(s, i);
-				i = 1;
-				reversed_list = reverse_list(create_list(s, i));
+				i = 0;
+				list = create_list(s, i, depth);
+				i = 0;
 				cout << "\n";
+				reversed_list = reverse_list(create_list(s, i, depth), depth);
+				cout << "\nИтоговый ответ: ";
+				if (s.length() != 1) {
+					cout << "(";
+				}
 				print_list(list, depth);
-				cout << "\n";
+				if (s.length() == 1) {
+					cout << " -> ";
+				}
+				else {
+					cout << ") -> (";
+				}
 				print_list(reversed_list, depth);
+				if (s.length() != 1) {
+					cout << ")\n\n";
+				}
+				else {
+					cout << "\n\n";
+				}
 				delete_list(list);
 				delete_list(reversed_list);
 			}
-			else if(s != ""){
+			else if (s != "") {
 				//Если строка не пустая, но записана неверно, происходит ошибка
 				cout << "Список записан некорректно!\n";
 			}
 			s = "";
 			getline(cin, s);
-			i = 0;
 		}
 	}
-	else{
+	else {
 		cout << "Неизвестная команда, выход из программы.\n";
 	}
 	return 0;
