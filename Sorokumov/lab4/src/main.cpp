3b1fec0d452d4d576968b92ad7ff9543b226a0ac
Homry <Sorokumov.g.2019@fml31.ru>
2020-10-28 11:00:35 +0300

    update lab_4

--- Sorokumov/lab4/src/main.cpp
+++ Sorokumov/lab4/src/main.cpp
@@ -5,31 +5,31 @@
 #include <vector>
 
 template <typename T>
-void Shell(T* A, int n){ //сортировка Шелла
+void shell(T* A, int n){ //сортировка Шелла
     int count, d;
     d=n/2; // Присвоение в d половины длины массива
     while (d>0){ // выполнение цикла пока d > 0
         for (int i=0; i<n-d; i++){
             std::cout << std::endl;
-            std::cout << "Проход " << i+1  << ", d = " << d << std::endl;
+            std::cout << "Passage " << i+1  << ", d = " << d << std::endl;
             for (int j = i; j >= 0; j--){
-                std::cout << "Сравнение элементов " << A[j] << " и " << A[j + d] << std::endl;
+                std::cout << "Comparison of elements " << A[j] << " и " << A[j + d] << std::endl;
                 if (A[j]>A[j+d]){ // сравнение двух элементов
                     /*
                      * Замена элементов если условие верно
                      */
-                    std::cout << "Замена элементов " << A[j] << " и " << A[j + d] << std::endl;
+                    std::cout << "Replacing elements " << A[j] << " и " << A[j + d] << std::endl;
                     count = A[j];
                     A[j] = A[j + d];
                     A[j + d] = count;
-                    std::cout << "Массив после замены: ";
+                    std::cout << "Array after replacement: ";
                     for (int k = 0; k < n; k++){
                         std::cout << A[k] << ' ';
                     }
                     std::cout << std::endl<<std::endl;
                 }
                 else{
-                    std::cout  << A[j] << " и " << A[j + d] << " замена не требуется " << std::endl;
+                    std::cout  << A[j] << " и " << A[j + d] << " no replacement required " << std::endl;
                 }
                 //std::cout << std::endl;
             }
@@ -45,7 +45,7 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     int n;
     bool file;
-    std::cout << "Напишите f - если чтение из файла, любой другой - если консоль" << std::endl;
+    std::cout << "Write f - if reading from file, any other - if console" << std::endl;
     char symbol;
     std::cin >> symbol;
     std::string input;
@@ -54,12 +54,15 @@ int main(){
      * Считывание размера массива из файла
      */
     if (symbol == 'f'){
-        std::cout << "Введите путь до файла:" << std::endl;
+        std::cout << "Enter the path to the file:" << std::endl;
         std::cin >> input;
         std::ifstream fin;
         fin.open(input);
         if (!fin){
-            std::cout << "error";
+            std::cout << "error" << std::endl;
+            system("pause");
+            std::cout << "To continue the program, press any key ...";
+            std::cin.get();
             exit(1);
         }
         fin >> n;
@@ -67,12 +70,15 @@ int main(){
         /*
          * Считывание размера массива из консоли
          */
-        std::cout<<"Размер массива > " ;
+        std::cout<<"Array size > " ;
         std::cin>>n;
         std::cout << std::endl;
     }
     if (n <= 0){
-        std::cout << "Некоректная длина";
+        std::cout << "Incorrect length" << std::endl;
+        system("pause");
+        std::cout << "To continue the program, press any key ...";
+        std::cin.get();
         exit(1);
     }
 
@@ -87,9 +93,9 @@ int main(){
         fin.open(input);
         fin >> n;
         for (int i=0; i<n; i++){
-             fin>>arrayForShell[i];
+            fin>>arrayForShell[i];
         }
-        std::cout << "Поданный массив: ";
+        std::cout << "The array fed: ";
         for (int i=0; i<n; i++){
             std::cout << arrayForShell[i] << ' ';
         }
@@ -99,7 +105,7 @@ int main(){
         * Считывание массива из консоли
         */
         for (int i=0; i<n; i++){
-            std::cout<<i+1<<" элемент >"; std::cin>>arrayForShell[i];
+            std::cout<<i+1<<" item > " << std::endl; std::cin>>arrayForShell[i];
         }
 
     }
@@ -110,28 +116,27 @@ int main(){
         arrayForStdSort.push_back(arrayForShell[i]);
     }
 
-    std::cout << std::endl << "Вызов сортировки Шелла" << std::endl << std::endl;
-    Shell(arrayForShell, n); //Вызов сортировки Шелла
+    std::cout << std::endl << "Shell sort call" << std::endl << std::endl;
+    shell(arrayForShell, n); //Вызов сортировки Шелла
 
     /*
      * Вывод отсортированного массива
      */
-    std::cout<<"Результирующий массив: ";
+    std::cout<<"Resulting array: ";
     for (int i=0; i<n; i++) {
         std::cout<<arrayForShell[i]<<" "; //вывод массива
     }
     std::cout << std::endl;
-    std::cout << std::endl << "Вызов стандартной сортировки" << std::endl;
+    std::cout << std::endl << "Calling the standard sort " << std::endl;
     std::sort(arrayForStdSort.begin(), arrayForStdSort.end()); //Вызов стандартной сортировки
-    std::cout<<"Массив после стандартной сортировки: ";
+    std::cout<<"Array after standard sort: ";
     for (int i=0; i<n; i++) {
         std::cout<<arrayForStdSort[i]<<" "; //вывод массива
     }
     std::cout << std::endl;
     delete [] arrayForShell; //освобождение памяти
     system("pause");
-    std::cout << "Для продолжения работы программы нажмите любую клавишу...";
+    std::cout << "To continue the program, press any key ...";
     std::cin.get();
     return 0;
 }
-
