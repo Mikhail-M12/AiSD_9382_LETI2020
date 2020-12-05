051ef7fb88f465c102cea47a85e8cabf0a1674b0
Роман Павлов <55055449+Crunchy0@users.noreply.github.com>
2020-12-05 14:46:47 +0300

    lab5 - Pavlov fix

--- Pavlov/lab5/5laba.cpp
+++ Pavlov/lab5/5laba.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Пара значений (необходимо для разных типов данных)
 template <typename T> struct Pair {
 	T x;
 	T y;
@@ -11,6 +12,7 @@ template <typename T> struct Pair {
 	Pair(T x, T y) : x(x), y(y) {}
 };
 
+//Узел - элемент дерева
 struct Node {
 	Pair<int> p;
 	Node* l, * r;
@@ -19,6 +21,7 @@ struct Node {
 	Node(int x, int y, Node* l, Node* r) : p(Pair<int>(x, y)), l(l), r(r) {}
 };
 
+//Запись в файл значений различных типов
 template <typename T>
 void writeToFile(const string filename, const T arg) {
 	ofstream output;
@@ -27,12 +30,14 @@ void writeToFile(const string filename, const T arg) {
 	output.close();
 }
 
+//Отступ (нужен для рекурсивного вывода)
 void indent(int n) {
 	for (int i = 0; i < n; i++) {
 		cout << "\t";
 	}
 }
 
+//Освобождение памяти (удаление дерева)
 void dispose(Node* t) {
 	if (t == nullptr) {
 		return;
@@ -42,6 +47,7 @@ void dispose(Node* t) {
 	delete t;
 }
 
+//Проверка на вхождение элемента с заданным ключом в дерево
 bool isIn(Node* t, int k) {
 	if (t != nullptr) {
 		if (k > t->p.x) {
@@ -55,11 +61,12 @@ bool isIn(Node* t, int k) {
 	return false;
 }
 
+//Слияние двух деревьев
 Node* merge(Node* l, Node* r, int& depth) {
 	if (!depth) {
 		cout << "\nAttempting to merge trees\n";
 	}
-	if (l == nullptr) {
+	if (l == nullptr) {			//Если одно из деревьев - nullptr, возвращаем другое (тоже может быть nullptr)
 		return r;
 	}
 	if (r == nullptr) {
@@ -67,6 +74,9 @@ Node* merge(Node* l, Node* r, int& depth) {
 	}
 
 	if (l->p.y > r->p.y) {
+		//Если у левого дерева приоритет больше, возвращаем его корень,
+		//предварительно рекурсивно вызвав функцию слияния для его правого поддерева,
+		//сохраняя неизменным исходное правое дерево.
 		indent(depth);
 		cout << "Merging with the right branch of  " << l->p.x << "  " << l->p.y << "\n";
 		depth++;
@@ -74,6 +84,9 @@ Node* merge(Node* l, Node* r, int& depth) {
 		depth--;
 		return l;
 	}
+	//Иначе возвращем корень правого дерева, перед этим
+	//выполнив аналогичные действия с его левым поддеревом и
+	//сохраняя исходное левое дерево
 	indent(depth);
 	cout << "Merging with the left branch of  " << r->p.x << "  " << r->p.y << "\n";
 	depth++;
@@ -82,6 +95,7 @@ Node* merge(Node* l, Node* r, int& depth) {
 	return r;
 }
 
+//Разбиение дерева на два поддерева
 Pair<Node*> split(Node* t, int k, int& depth) {
 	if (!depth) {
 		cout << "\nAttempting to split the tree.\n";
@@ -90,6 +104,8 @@ Pair<Node*> split(Node* t, int k, int& depth) {
 		return Pair<Node*>(nullptr, nullptr);
 	}
 	else if (k > t->p.x) {
+		//Если заданный ключ больше ключа текущего элемента, спускаемся по правой ветви
+		//и возвращаем пару значений - исходный элемент и правое поддерево результата разбиения
 		indent(depth);
 		cout << "Descending to  the right branch of  " << t->p.x << "  " << t->p.y << "\n";
 		depth++;
@@ -99,6 +115,8 @@ Pair<Node*> split(Node* t, int k, int& depth) {
 		return Pair<Node*>(t, nPair.y);
 	}
 	else{
+		//Иначе спускаемся по левой ветви и возвращаем левое поддерево результата разбиения
+		//и исходный элемент
 		indent(depth);
 		cout << "Descending to  the left branch of  " << t->p.x << "  " << t->p.y << "\n";
 		depth++;
@@ -110,14 +128,18 @@ Pair<Node*> split(Node* t, int k, int& depth) {
 	return Pair<Node*>(t, nullptr);
 }
 
+
+//Добавление заданного элемента
 Node* add(Node* t, int k, int v) {
 	int depth = 0;
 	cout << "\n\nSplitting the treap by key " << k << "\n";
-	Pair<Node*> p = split(t, k, depth);
+	Pair<Node*> p = split(t, k, depth);		//Разбиение по заданному ключу
 	Node* n = new Node(k, v);
-	return merge(merge(p.x, n, depth), p.y, depth);
+	return merge(merge(p.x, n, depth), p.y, depth);	//Слияние левого поддерева результата разбиения
+													//с новым элементом, а затем с правым поддеревом
 }
 
+//Удаление элемента из дерева
 Node* remove(Node* t, int k, int& depth) {
 	if (!depth) {
 		cout << "\nAttempting to remove existing element.\n";
@@ -126,18 +148,21 @@ Node* remove(Node* t, int k, int& depth) {
 		indent(depth);
 		cout << "Current element: " << t->p.x << "  " << t->p.y << "\n";
 		if (t->p.x < k) {
+			//Если заданный ключ больше, спускаемся к правому поддереву
 			depth++;
 			t->r = remove(t->r, k, depth);
 			depth--;
 			return t;
 		}
 		else if (t->p.x > k) {
+			//Если меньше - к левому поддереву
 			depth++;
 			t->l = remove(t->l, k, depth);
 			depth--;
 			return t;
 		}
 		else {
+			//Если нашёлся данный ключ, удаляем его, сливая затем его поддеревья
 			indent(depth);
 			cout << "\nRequired element found: " << t->p.x << "  " << t->p.y << " , removing...\n";
 			Node* l = t->l, * r = t->r;
@@ -147,19 +172,25 @@ Node* remove(Node* t, int k, int& depth) {
 			return merge(l, r, depth);
 		}
 	}
+	//Если t = nullptr
 	return t;
 }
 
+
+//Вывод дерева на экран
 void printTreap(Node* t, int& depth) {
 	if (t != nullptr) {
 		if (t->r != nullptr) {
+			//П
 			depth++;
 			printTreap(t->r, depth);
 			depth--;
 		}
+		//К
 		indent(depth);
 		cout << t->p.x << "  " << t->p.y << "\n";
 		if (t->l != nullptr) {
+			//Л
 			depth++;
 			printTreap(t->l, depth);
 			depth--;
@@ -167,16 +198,20 @@ void printTreap(Node* t, int& depth) {
 	}
 }
 
+//Печать элементов дерева в файл по возрастанию ключей
 void printAscendingToFile(Node* t, const string& filename) {
 	if (t != nullptr) {
 		if (t->l != nullptr) {
+			//Л
 			printAscendingToFile(t->l, filename);
 		}
+		//К
 		writeToFile(filename, t->p.x);
 		writeToFile(filename, "  ");
 		writeToFile(filename, t->p.y);
 		writeToFile(filename,  '\n');
 		if (t->r != nullptr) {
+			//П
 			printAscendingToFile(t->r, filename);
 		}
 	}
@@ -197,10 +232,12 @@ int main() {
 		ifstream in;
 		ofstream out;
 
+		//Открыть файл на перезапись
 		out.open(output_filename);
 		out << "";
 		out.close();
 
+		//Ввод имени файла со входными данными
 		cout << "Enter the input file name: \n\n";
 		cin >> input_filename;
 		in.open(input_filename);
@@ -208,29 +245,37 @@ int main() {
 		if (in.is_open()) {
 			while (!in.eof()) {
 				if (!(in >> key) || !(in >> value)) {
+					//Если не удалось считать пару ключ-значение
 					break;
 				}
 				if (isIn(treap, key)) {
+					//Если элемент с таким ключом уже есть, убрать
 					cout << "\n\nElement with key = " << key << " already exists. Replacing...";
 					treap = remove(treap, key, depth);
 				}
+				//Добавление элемента
 				cout << "\n\nAdding new element with key = " << key << " to the treap\n";
 				treap = add(treap, key, value);
 			}
 			if (treap != nullptr) {
+				//Если дерево не пустое, вывести необходимые данные
 				printTreap(treap, depth);
 				printAscendingToFile(treap, output_filename);
 				cout << "\nCheck out the results in \"output.txt\"\n";
 			}
 			else {
-				cout << "File is empty or contains inappropriate data.\n";
+				//Если файл пуст или данные некорректны
+				cout << "File is empty or contains incorrect data.\n";
+				writeToFile(output_filename, "File is empty or contains incorrect data.\n");
 			}
 		}
 		else {
+			//Если не удалось открыть файл
 			cout << input_filename << " doesn't exist!\n";
 		}
 	}
 	else if (command == 1) {
+		//Те же действия, только ввод и вывод осуществляются в консоли
 		cout << "Enter the pairs (key then value, both integer) or anything except that (to finish): \n\n";
 		while ((cin >> key)) {
 			if (!(cin >> value)) {
