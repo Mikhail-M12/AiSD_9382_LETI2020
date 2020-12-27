370a22ee395a53f411364ef22228ca6418934849
Valentina-Gol <vip.valya.golubeva@mail.ru>
2020-12-27 12:25:08 +0300

    add lab1

---
+++ Golubeva/lab1/name.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <limits>
+#include <cctype> 
+
+using namespace std;
+
+void change_elements(int& , int& );
+bool my_isdigit(string );
+void my_permutation(string , int* , int , int);
