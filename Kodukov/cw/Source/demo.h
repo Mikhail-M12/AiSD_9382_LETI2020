5120c2f4db6cd3e020886ed96154bade8031724b
alexundertable <akodukovich@mail.ru>
2020-12-28 13:28:52 +0300

    lab1

--- Kodukov/cw/Source/demo.h
+++
@@ -1,9 +0,0 @@
-#include "code.h"
-
-void PrintStatistics();
-
-void PrintCodeBuilding();
-
-void PrintTreeBuilding(Tree *(*buildCodeTree)(ElemArr curFreq, bool output));
-
-void PrintDecoding();
--- Kodukov/cw/Source/fanohuffman.h
+++
@@ -1,9 +0,0 @@
-#ifndef FANOHUFFMAN_H_
-#define FANOHUFFMAN_H_
-
-#include "def.h"
-
-Tree *buildCodeTreeFano(ElemArr curFreq, bool output);
-Tree *buildCodeTreeHuffman(ElemArr curFreq, bool output);
-
-#endif //FANOHUFFMAN_H_
